b9528dc8c7cf8faef2e3c5e1df2fc540fc65e02f
italolelis <italolelis@gmail.com>
2018-07-13 00:26:05 +0200

    Brew formula update for reachable version 0.0.5

--- Formula/reachable.rb
+++ Formula/reachable.rb
@@ -1,9 +1,9 @@
 class Reachable < Formula
   desc "A CLI tool to check if a domain is up"
   homepage "https://github.com/italolelis/reachable"
-  url "https://github.com/italolelis/reachable/releases/download/0.0.3/reachable_0.0.3_darwin_amd64.tar.gz"
-  version "0.0.3"
-  sha256 "1130111690efdfb7e5e7afce6d47e4183d0486edf4aa338de0b5813d892c2cd8"
+  url "https://github.com/italolelis/reachable/releases/download/0.0.5/reachable_0.0.5_darwin_amd64.tar.gz"
+  version "0.0.5"
+  sha256 "9770d67ff826abd0ec8c6b4c97019f234a1f8cec2f19ce5f2d1f0a41b15dda40"
 
   def install
     bin.install "reachable"
